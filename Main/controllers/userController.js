b561768f286afe26a006417a89639388a70cd688
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-04-19 23:11:21 -0500

    readme and cleanup commit

--- Main/controllers/userController.js
+++ Main/controllers/userController.js
@@ -1,28 +1,6 @@
 const { ObjectId } = require('mongoose').Types;
 const { Reaction, Thought, User } = require('../models');
 
-// // Aggregate function to get the number of students overall
-// const headCount = async () =>
-//   Student.aggregate()
-//     .count('studentCount')
-//     .then((numberOfStudents) => numberOfStudents);
-
-// // Aggregate function for getting the overall grade using $avg
-// const grade = async (studentId) =>
-//   Student.aggregate([
-//     // only include the given student by using $match
-//     { $match: { _id: ObjectId(studentId) } },
-//     {
-//       $unwind: '$assignments',
-//     },
-//     {
-//       $group: {
-//         _id: ObjectId(studentId),
-//         overallGrade: { $avg: '$assignments.score' },
-//       },
-//     },
-//   ]);
-
 module.exports = {
   // Get all users
   getUsers(req, res) {
